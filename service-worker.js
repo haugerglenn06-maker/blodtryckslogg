df313695e3c91ce08a37c33f89bb5e469feedad3
haugerglenn06-maker <haugerglenn06@gmail.com>
2025-12-16 14:36:15 +0100

    Force cache refresh v1.4.2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bp-logg-v1.4.0';
+const CACHE_NAME = 'bp-logg-v1.4.2';
 
 const FILES_TO_CACHE = [
   './',
@@ -31,3 +31,4 @@ self.addEventListener('fetch', e => {
     caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
+
